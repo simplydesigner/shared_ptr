b76dc223fe2694dd82d7ffd624044c676086d58b
simplydesigner <simplydesigner@ya.ru>
2016-09-04 07:12:18 +0300

    Initial Commit

---
+++ shared_ptr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  shared_ptr
+//
+//  Created by simplydesigner on 04/09/16.
+//  Copyright © 2016 JustDevelopers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
